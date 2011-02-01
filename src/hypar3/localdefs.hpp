177296953ae0287fafffc4ccc5a388f0ca8735a8
Ravindra R. Jaju <jaju@msync.org>
2011-02-01 15:50:33 +0530

    Initial bare-bones - project start

---
+++ src/hypar3/localdefs.hpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2004 Ravindra Jaju (jaju [AT] msync org)
+ */
+
+/*
+ * This file is part of the HyParSuite and usage is governed by the License
+ * included with the source-code
+ */
+
+#pragma once
+#ifdef HAVE_CONFIG_H
+# include "hypar/config.h"
+#endif
+
+#ifdef __cplusplus
+#  define BEGIN_C_DECLS extern "C" {
+#  define END_C_DECLS   }
+#  define BEGIN_NAMESPACE(x) namespace x {
+#  define END_NAMESPACE(x) }
+#  define USING_NAMESPACE(x) using namespace x
+#  define safe_delete(x) if (x) { delete x; x = 0; }
+#else /* !__cplusplus */
+#  define BEGIN_C_DECLS
+#  define END_C_DECLS
+#  define BEGIN_NAMESPACE(x)
+#  define END_NAMESPACE(x)
+#  define USING_NAMESPACE(x)
+#endif /* __cplusplus */
+
+#ifdef __GNUC__
+# define likely(x) __builtin_expect(!!(x), 1)
+# define unlikely(x)  __builtin_expect(!!(x), 0)
+#else
+# define likely(x) (x)
+# define unlikely(x) (x)
+#endif
