8bc0cbeb41316537f110a1f46ead6fe6ed671c62
Ravindra R. Jaju <jaju@msync.org>
2011-03-05 19:57:41 +0000

    rudimentary shell-script to kick of a new project

---
+++ create-project.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+usage() {
+  echo "Usage: $0 <project-name> <base-directory>"
+}
+
+updatename() {
+  tempfile=`tempfile`
+  for f in `find . -type f`; do
+    cat $f | sed s/$SKELNAME/$PROJECTNAME/g > $tempfile
+    mv $tempfile $f
+  done
+}
+
+SKELNAME=skel_project
+if [ "x$2" = "x" ]; then
+  usage
+  exit 1
+fi
+
+PROJECTNAME=$1
+DESTDIR=$2
+
+if [ ! -d "$DESTDIR" ]; then
+  echo "Directory "$DESTDIR" does not exist! Create it first. Exiting..."
+  exit 1
+fi
+
+FINALDESTINATION="$DESTDIR/$PROJECTNAME"
+mkdir -p "$FINALDESTINATION"
+
+cp -af * $FINALDESTINATION && cd $FINALDESTINATION && rm -rf .git && updatename
