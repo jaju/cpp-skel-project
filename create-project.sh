4c63da0bc408b33f34e92064354c88bdadd1fe72
Ravindra R. Jaju <jaju@msync.org>
2011-04-09 12:26:40 +0530

    Updated create-project script to only copy files of interest by passing the right options to rsync. The script itself too is copied as of now.

--- create-project.sh
+++ create-project.sh
@@ -4,6 +4,18 @@ usage() {
   echo "Usage: $0 <project-name> <base-directory>"
 }
 
+copy_skeleton() {
+  echo "HEIJEWEHWJEH"
+  RSYNCINCLUDES=`mktemp`
+  RSYNCEXCLUDES=`mktemp`
+  git ls-files > $RSYNCINCLUDES
+  echo '*' > $RSYNCEXCLUDES
+  cat $RSYNCINCLUDES
+  rsync --include-from=$RSYNCINCLUDES $1
+  rsync -ar --files-from=$RSYNCINCLUDES --include-from=$RSYNCINCLUDES --exclude-from=$RSYNCEXCLUDES . $1
+  rm -f $RSYNCINCLUDES $RSYNCEXCLUDES
+}
+
 cleanup() {
   echo "Removing .git directory and copy of this ($0) script..."
   cd $FINALDESTINATION && rm -rf .git && rm -rf `basename $0` && cd -
@@ -12,7 +24,7 @@ cleanup() {
 updatename() {
   echo "Updating files - renaming all occurences of $SKELNAME with $PROJECTNAME..."
   cd $FINALDESTINATION
-  tempfile=`tempfile`
+  tempfile=`mktemp`
   for f in `find . -type f`; do
     cat $f | sed s/$SKELNAME/$PROJECTNAME/g > $tempfile
     mv $tempfile $f
@@ -38,4 +50,6 @@ FINALDESTINATION="$DESTDIR/$PROJECTNAME"
 mkdir -p "$FINALDESTINATION"
 
 echo "Creating copy of skeleton at $FINALDESTINATION..."
-rsync -cavzu ./ $FINALDESTINATION/ &>/dev/null && cleanup && updatename
+copy_skeleton $FINALDESTINATION/ && updatename && echo done && exit 0
+echo "Something went bad."
+exit 1
